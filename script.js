a01f551f7c44c481f3d1aaa29f9861bc815ce99f
Sansshrek <franci.pagliaccia13@gmail.com>
2022-02-18 14:38:37 +0100

    style changes

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var check_test = false;
 function generate_word(){
     index = Math.floor(Math.random() * random_words.length);
     final_word = random_words[index];
+    final_word = "GAMBE"
     console.log(final_word);
 }
 
@@ -26,6 +27,31 @@ function input(letter){
             }
         }
     }
+}
+
+function reset(){
+    all_row = document.getElementsByClassName("input-row");
+    for(var i=0; i<all_row.length; i++){
+        box_div = all_row[i].getElementsByClassName("input-box");
+        for(var j=0; j<box_div.length; j++){
+            var div = box_div[j];
+            if(div.dataset.state == "full"){
+                div.innerHTML = "";
+                div.dataset.state = "empty";
+                div.classList.remove("green");
+                div.classList.remove("yellow");
+            }
+        }
+    }
+    all_key = document.getElementsByClassName("input-key");
+    for(var i=0; i<all_key.length; i++){
+        all_key[i].classList.remove("green");
+        all_key[i].classList.remove("yellow");
+        all_key[i].classList.remove("gray");
+    }
+    document.getElementById("result-div").style.visibility = "hidden";
+    cont_letter = cont_row = 0;
+    generate_word()
 }
 
 function del(){
@@ -108,11 +134,13 @@ function check_word(word){
         box_obj[final_word[i]]++;
     }
     for(var i=0; i<5; i++){
-        if(document.getElementById(box[i].textContent).style.backgroundColor != "rgb(4, 170, 109)");
-            document.getElementById(gray_letters[i]).style.backgroundColor = gray_color;
+        if(document.getElementById(box[i].textContent).classList.contains("green") == false)
+            document.getElementById(gray_letters[i]).classList.add("gray");
         if(box[i].textContent == final_word[i]){
-            box[i].style.background = green_color;
-            document.getElementById(box[i].textContent).style.backgroundColor = green_color;
+            box[i].classList.add("green")
+            document.getElementById(box[i].textContent).classList.remove("gray");
+            document.getElementById(box[i].textContent).classList.remove("yellow");
+            document.getElementById(box[i].textContent).classList.add("green");
             box_obj[box[i].textContent]--;
         }
     }
@@ -120,10 +148,11 @@ function check_word(word){
     for(var i=0; i<5; i++){
         for(var j=0; j<5; j++){
             if(box[i].textContent == final_word[j]){
-                if(document.getElementById(box[i].textContent).style.backgroundColor != "rgb(4, 170, 109)"){
-                    document.getElementById(box[i].textContent).style.backgroundColor = yellow_color;
+                if(document.getElementById(box[i].textContent).classList.contains("green") == false){
+                    document.getElementById(box[i].textContent).classList.remove("gray");
+                    document.getElementById(box[i].textContent).classList.add("yellow")
                     if(box_obj[box[i].textContent] != 0){
-                        box[i].style.background = yellow_color;
+                        box[i].classList.add("yellow");
                         box_obj[box[i].textContent]--;
                     }
                 }
@@ -151,6 +180,7 @@ function ent(){
             if(final_word == word){
                 document.getElementById("result-div").style.visibility = "visible";
                 document.getElementById("result-div").style.backgroundColor = "#04aa6d";
+                document.getElementsByClassName("reload-button")[0].style.backgroundColor = "#04aa6d";
                 document.getElementsByClassName("result-message")[0].innerHTML = "HAI VINTO!";
                 document.getElementsByClassName("correct-answer")[0].innerHTML = "La parola giusta era "+final_word;
                 cont_row = 6;
@@ -160,6 +190,7 @@ function ent(){
                 if(cont_row == 6){
                     document.getElementById("result-div").style.visibility = "visible";
                     document.getElementById("result-div").style.backgroundColor = "#d63e2d";
+                    document.getElementsByClassName("reload-button")[0].style.backgroundColor = "#d63e2d";
                     document.getElementsByClassName("result-message")[0].innerHTML = "HAI PERSO!";
                     document.getElementsByClassName("correct-answer")[0].innerHTML = "La parola giusta era "+final_word;
                 }
@@ -195,73 +226,4 @@ function show_info(){
 
 function close_info(){
     document.getElementsByClassName("instructions")[0].style.visibility = "hidden";
-}
-
-function test(){
-    word = get_word()
-    test_word_list = []
-    test_word_obj_list = []
-    if(check_test_type == "B")
-        test_word_list = words_list
-    else
-        test_word_list = random_words
-
-
-    for(test_word in test_word_list){
-        box_obj = {
-            A: 0,
-            B: 0,
-            C: 0,
-            D: 0,
-            E: 0,
-            F: 0,
-            G: 0,
-            H: 0,
-            I: 0,
-            J: 0,
-            K: 0,
-            L: 0,
-            M: 0,
-            N: 0,
-            O: 0,
-            P: 0,
-            Q: 0,
-            R: 0,
-            S: 0,
-            T: 0,
-            U: 0,
-            V: 0,
-            W: 0,
-            X: 0,
-            Y: 0,
-            Z: 0
-        }
-        word_value = 0
-        for(var i=0; i<5; i++){
-            box_obj[word[i]]++
-        }
-        for(var i=0; i<5; i++){
-            if(test_word[i] == word[i]){
-                box_obj[test_word[i]]--
-                word_value++
-            }
-        }
-        
-        for(var i=0; i<5; i++){
-            for(var j=0; j<5; j++){
-                if(test_word[i] == word[j]){
-                    if(box_obj[test_word[i]] != 0){
-                        box_obj[test_word[i]]--
-                        word_value += 0.2
-                    }
-                }
-            }
-        }
-        
-        test_word_obj_list.push(test_obj)
-    }
-
-    var table = document.getElementById("table")
-    var row = table.insertRow(0)
-    var new_cell = row.insertCell(0)
 }
